64f73f2826c23385a8ba9f72f36c1e5df15cc1ae
esmaNurocal <esmanurocal4@gmail.com>
2022-05-16 11:37:53 +0300

    deneme2

--- Library/Library/deneme.Designer.cs
+++ Library/Library/deneme.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "deneme formu";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "deneme2";
+            // 
             // deneme
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "deneme";
             this.Text = "deneme";
@@ -56,5 +67,6 @@
         #endregion
 
         private Label label1;
+        private Label label2;
     }
 }
