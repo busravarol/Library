917437f16f7a5b68319235136136edc3cdf58b1b
enesalpman <enesalpman@hotmail.com>
2022-05-16 11:42:25 +0300

    gitıgnıre güncellendi

--- Library/Library/deneme.Designer.cs
+++ Library/Library/deneme.Designer.cs
@@ -35,28 +35,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(288, 172);
+            this.label1.Location = new System.Drawing.Point(252, 129);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.Size = new System.Drawing.Size(86, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "deneme formu";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(288, 231);
+            this.label2.Location = new System.Drawing.Point(252, 173);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.Size = new System.Drawing.Size(56, 15);
             this.label2.TabIndex = 1;
-            this.label2.Text = "deneme2";
+            this.label2.Text = "deneme3";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // deneme
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "deneme";
             this.Text = "deneme";
             this.ResumeLayout(false);
