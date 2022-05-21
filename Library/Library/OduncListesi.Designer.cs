2cc570563da2e87abab6232e95cfac4edb73d1a5
esmaNurocal <esmanurocal4@gmail.com>
2022-05-21 21:15:48 +0300

    odunc listesi güncellendi
    (iikavak)

--- Library/Library/OduncListesi.Designer.cs
+++ Library/Library/OduncListesi.Designer.cs
@@ -40,40 +40,39 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(144, 41);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(165, 55);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(770, 261);
+            this.dataGridView1.Size = new System.Drawing.Size(880, 348);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(144, 343);
-            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView2.Location = new System.Drawing.Point(165, 457);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 29;
-            this.dataGridView2.Size = new System.Drawing.Size(770, 230);
+            this.dataGridView2.Size = new System.Drawing.Size(880, 307);
             this.dataGridView2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(443, 14);
+            this.label1.Location = new System.Drawing.Point(506, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.Size = new System.Drawing.Size(156, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Teslim Verilen Kitaplar";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(443, 314);
+            this.label2.Location = new System.Drawing.Point(506, 419);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(108, 15);
+            this.label2.Size = new System.Drawing.Size(138, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Geri Alınan Kitaplar";
             // 
@@ -86,15 +85,14 @@
             // 
             // OduncListesi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(138)))), ((int)(((byte)(185)))));
-            this.ClientSize = new System.Drawing.Size(1102, 562);
+            this.ClientSize = new System.Drawing.Size(1259, 749);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "OduncListesi";
             this.Text = "OduncListesi";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- Library/Library/OduncListesi.cs
+++ Library/Library/OduncListesi.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Library
 {
@@ -15,6 +16,19 @@ namespace Library
         public OduncListesi()
         {
             InitializeComponent();
+        }
+        MySqlConnection baglanti = new MySqlConnection("Server=172.21.54.3; Uid=Banipal; pwd=Banipal12345.; database=Banipal;");
+       
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string query = "select * from tbl_odunc";
+            using(MySqlDataAdapter adapter =new MySqlDataAdapter(query, baglanti))
+            {
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource=ds.Tables[0];
+            }
         }
     }
 }
