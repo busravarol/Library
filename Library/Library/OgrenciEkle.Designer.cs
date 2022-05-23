37c1c24ed170c9ab8ad333fa45ba2a9f5e954873
busravarol <busravarrol@gmail.com>
2022-05-23 23:26:44 +0300

    Backup.

--- Library/Library/OgrenciEkle.Designer.cs
+++ Library/Library/OgrenciEkle.Designer.cs
@@ -34,55 +34,144 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Ad = new System.Windows.Forms.Label();
+            this.Soyad = new System.Windows.Forms.Label();
+            this.OgrenciNo = new System.Windows.Forms.Label();
+            this.BolumAdi = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.Telefon = new System.Windows.Forms.Label();
+            this.btnOgrenciEkle = new System.Windows.Forms.Button();
+            this.btnİptal = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(184, 70);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(165, 23);
+            this.textBox1.Size = new System.Drawing.Size(172, 23);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(184, 99);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(165, 23);
+            this.textBox2.Size = new System.Drawing.Size(172, 23);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(184, 128);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(165, 23);
+            this.textBox3.Size = new System.Drawing.Size(172, 23);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
             this.textBox4.Location = new System.Drawing.Point(184, 157);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(165, 23);
+            this.textBox4.Size = new System.Drawing.Size(172, 23);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
             this.textBox5.Location = new System.Drawing.Point(184, 186);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(165, 23);
+            this.textBox5.Size = new System.Drawing.Size(172, 23);
             this.textBox5.TabIndex = 4;
             // 
             // textBox6
             // 
             this.textBox6.Location = new System.Drawing.Point(184, 215);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(165, 23);
+            this.textBox6.Size = new System.Drawing.Size(172, 23);
             this.textBox6.TabIndex = 5;
+            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(96, 73);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(22, 15);
+            this.Ad.TabIndex = 6;
+            this.Ad.Text = "Ad";
+            // 
+            // Soyad
+            // 
+            this.Soyad.AutoSize = true;
+            this.Soyad.Location = new System.Drawing.Point(96, 107);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(39, 15);
+            this.Soyad.TabIndex = 7;
+            this.Soyad.Text = "Soyad";
+            // 
+            // OgrenciNo
+            // 
+            this.OgrenciNo.AutoSize = true;
+            this.OgrenciNo.Location = new System.Drawing.Point(96, 136);
+            this.OgrenciNo.Name = "OgrenciNo";
+            this.OgrenciNo.Size = new System.Drawing.Size(68, 15);
+            this.OgrenciNo.TabIndex = 8;
+            this.OgrenciNo.Text = "Öğrenci No";
+            // 
+            // BolumAdi
+            // 
+            this.BolumAdi.AutoSize = true;
+            this.BolumAdi.Location = new System.Drawing.Point(96, 165);
+            this.BolumAdi.Name = "BolumAdi";
+            this.BolumAdi.Size = new System.Drawing.Size(63, 15);
+            this.BolumAdi.TabIndex = 9;
+            this.BolumAdi.Text = "Bölüm Adı";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(96, 194);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(36, 15);
+            this.Email.TabIndex = 10;
+            this.Email.Text = "Email";
+            // 
+            // Telefon
+            // 
+            this.Telefon.AutoSize = true;
+            this.Telefon.Location = new System.Drawing.Point(96, 223);
+            this.Telefon.Name = "Telefon";
+            this.Telefon.Size = new System.Drawing.Size(45, 15);
+            this.Telefon.TabIndex = 11;
+            this.Telefon.Text = "Telefon";
+            // 
+            // btnOgrenciEkle
+            // 
+            this.btnOgrenciEkle.Location = new System.Drawing.Point(184, 267);
+            this.btnOgrenciEkle.Name = "btnOgrenciEkle";
+            this.btnOgrenciEkle.Size = new System.Drawing.Size(83, 47);
+            this.btnOgrenciEkle.TabIndex = 12;
+            this.btnOgrenciEkle.Text = "Ekle";
+            this.btnOgrenciEkle.UseVisualStyleBackColor = true;
+            this.btnOgrenciEkle.Click += new System.EventHandler(this.btnOgrenciEkle_Click);
+            // 
+            // btnİptal
+            // 
+            this.btnİptal.Location = new System.Drawing.Point(273, 267);
+            this.btnİptal.Name = "btnİptal";
+            this.btnİptal.Size = new System.Drawing.Size(83, 47);
+            this.btnİptal.TabIndex = 13;
+            this.btnİptal.Text = "İptal";
+            this.btnİptal.UseVisualStyleBackColor = true;
             // 
             // OgrenciEkle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(459, 450);
+            this.Controls.Add(this.btnİptal);
+            this.Controls.Add(this.btnOgrenciEkle);
+            this.Controls.Add(this.Telefon);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.BolumAdi);
+            this.Controls.Add(this.OgrenciNo);
+            this.Controls.Add(this.Soyad);
+            this.Controls.Add(this.Ad);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -104,5 +193,13 @@
         private TextBox textBox4;
         private TextBox textBox5;
         private TextBox textBox6;
+        private Label Ad;
+        private Label Soyad;
+        private Label OgrenciNo;
+        private Label BolumAdi;
+        private Label Email;
+        private Label Telefon;
+        private Button btnOgrenciEkle;
+        private Button btnİptal;
     }
 }
--- Library/Library/OgrenciEkle.cs
+++ Library/Library/OgrenciEkle.cs
@@ -1,20 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using MySql.Data.MySqlClient;
+
+
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace Library
 {
     public partial class OgrenciEkle : Form
     {
+        MySqlConnection con;
         public OgrenciEkle()
         {
             InitializeComponent();
+            con = new MySqlConnection("Server=172.21.54.3; Uid=Banipal; pwd=Banipal12345.; database=Banipal;");
+        }
+
+        private void btnOgrenciEkle_Click(object sender, EventArgs e)
+        {
+            MySqlConnection baglanti = new MySqlConnection();
+            baglanti.ConnectionString =
+                "Server=172.21.54.3; Uid=Banipal; pwd=Banipal12345.; database=Banipal";
+            baglanti.Open();
+            MySqlCommand komutum = new MySqlCommand();
+            komutum.Connection = baglanti;
+            komutum.CommandText = "insert into Ogrenci(Ad, Soyad, OgrenciNo, Email, BolumAdi, Telefon) values (@Ad, @Soyad, @OgrenciNo, @Email, @BolumAdi, @Telefon)";
+            
+            string sorgu = "INSERT INTO Ogrenci (Ad, Soyad, OgrenciNo, Email, BolumAdi, Telefon,) VALUES('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text
++ "' ,'" + textBox5.Text + "', '" + textBox6.Text + "',0,0)";
+            komutum.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Öğrenci Kaydı Yapıldı.");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
